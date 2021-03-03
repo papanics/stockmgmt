34310330dc8acdb4354af688d0cbd1ca4634e0ec
jong <jongperono@gmail.com>
2021-03-03 16:35:21 +0800

    Update admin.py

--- stockmgmt/admin.py
+++ stockmgmt/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Stock
+from .forms import StockCreateForm
 
-admin.site.register(Stock)
+class StockCreateAdmin(admin.ModelAdmin):
+    list_display = ['category', 'item_name', 'quantity']
+    form = StockCreateForm
+    list_filter = ['category']
+    search_fields = ['category', 'item_name']
+
+admin.site.register(Stock, StockCreateAdmin)
