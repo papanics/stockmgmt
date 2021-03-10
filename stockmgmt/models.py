db5f96fa2f2e7a71c01f8fbc35f0f5de7d6a37e4
jong <jongperono@gmail.com>
2021-03-10 18:30:04 +0800

    update

--- stockmgmt/models.py
+++ stockmgmt/models.py
@@ -20,7 +20,7 @@ class Stock(models.Model):
 	created_by = models.CharField(max_length=50, blank=True, null=True)
 	reorder_level = models.IntegerField(default='0', blank=True, null=True)
 	last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False) 
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=True) 
 	
 	def __str__(self):
 		return self.item_name
